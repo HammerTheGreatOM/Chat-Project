284f397b6de32bfed6cdbb19d9fc702b2327703d
HammerTheGreatOM <hammermarlatt@gmail.com>
2026-03-27 19:06:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const Database = require('pg');
+const Database = require('better-sqlite3');
 const fs = require('fs');
 const path = require('path');
 
